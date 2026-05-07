71272443071683b09bce8620cb30ab4a1e062acc
MarcinWawrzyniak <marcin.wawrzyniak@nomagic.ai>
2026-05-07 13:50:42 +0200

    [None-Projects] TTB-0 udpate logging to log out if device was discovered using device_user_id or serial_no

--- pylon_camera/src/pylon_camera/pylon_camera_parameter.cpp
+++ pylon_camera/src/pylon_camera/pylon_camera_parameter.cpp
@@ -323,13 +323,17 @@ void PylonCameraParameter::adaptDeviceUserId(const ros::NodeHandle& nh, const st
 
 void PylonCameraParameter::validateParameterSet(const ros::NodeHandle& nh)
 {
-    if (!device_user_id_.empty())
+    if (!serial_no_.empty())
     {
-        ROS_INFO_STREAM("Trying to connect the camera device with the following device user id: " << this->device_user_id_.c_str());
+        ROS_INFO_STREAM("Trying to connect the camera device with serial number: " << serial_no_);
+    }
+    else if (!device_user_id_.empty())
+    {
+        ROS_INFO_STREAM("Trying to connect the camera device with device user id: " << device_user_id_);
     }
     else
     {
-        ROS_INFO_STREAM("No Device User ID set -> Will connect the first available camera device");
+        ROS_INFO_STREAM("No Serial Number or Device User ID set -> Will connect the first available camera device");
     }
 
     if (frame_rate_ < 0 && frame_rate_ != -1)
